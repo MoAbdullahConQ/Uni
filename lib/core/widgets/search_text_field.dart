8e8fdc62352b54a214f96fdec79f7b41570870f7
Mu Abdullah <mohamed553231@gmail.com>
2026-05-16 18:10:02 +0300

    feat(home): enhance HomeViewBody with search functionality and localized text

--- lib/core/widgets/search_text_field.dart
+++ lib/core/widgets/search_text_field.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:uni/core/utils/app_colors.dart';
+import 'package:uni/core/utils/app_images.dart';
 import 'package:uni/core/utils/app_text_style.dart';
 
 class SearchTextField extends StatelessWidget {
@@ -7,12 +10,18 @@ class SearchTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
+      padding: const EdgeInsets.only(left: 16),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 1.6, color: const Color(0x33154517)),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        shadows: [
           BoxShadow(
-            color: Color(0x0A000000),
-            blurRadius: 9,
-            offset: Offset(0, 2),
+            color: Color(0x19000000),
+            blurRadius: 3,
+            offset: Offset(0, 1),
             spreadRadius: 0,
           ),
         ],
@@ -22,15 +31,32 @@ class SearchTextField extends StatelessWidget {
         decoration: InputDecoration(
           prefixIcon: SizedBox(
             width: 20,
-            // child: Center(child: SvgPicture.asset(Assets.imagesSearch)),
+            child: Center(child: SvgPicture.asset(Assets.imagesIconSearch)),
           ),
-          suffixIcon: SizedBox(
+          suffixIcon: Container(
             width: 20,
-            // child: Center(child: SvgPicture.asset(Assets.imagesFilter)),
+            margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 4),
+            decoration: ShapeDecoration(
+              color: AppColors.primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              shadows: [
+                BoxShadow(
+                  color: AppColors.primaryColor.withOpacity(0.4),
+                  blurRadius: 15,
+                  offset: Offset(0, 10),
+                  spreadRadius: -3,
+                ),
+              ],
+            ),
+            child: Center(
+              child: SvgPicture.asset(Assets.imagesFilter, height: 22),
+            ),
           ),
-          hintText: 'ابحث عن.......',
-          hintStyle: TextStyles.regular13.copyWith(
-            color: const Color(0xFF949D9E),
+          hintText: 'دور عن جامعة، كلية، أو تخصص...',
+          hintStyle: TextStyles.regular16.copyWith(
+            color: const Color(0x66154517),
           ),
           border: buildBorder(),
           enabledBorder: buildBorder(),
@@ -44,7 +70,7 @@ class SearchTextField extends StatelessWidget {
 
   OutlineInputBorder buildBorder() {
     return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(4),
+      borderRadius: BorderRadius.circular(16),
       borderSide: BorderSide(color: Colors.white, width: 1),
     );
   }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -4,7 +4,6 @@ import 'package:uni/core/utils/app_colors.dart';
 import 'package:uni/core/utils/app_text_style.dart';
 import 'package:uni/core/widgets/search_text_field.dart';
 import 'package:uni/features/home/presentation/views/widgets/custom_home_app_bar.dart';
-import 'package:uni/features/home/presentation/views/widgets/faheem_banner_widget.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -19,7 +18,41 @@ class HomeViewBody extends StatelessWidget {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [const CustomHomeAppBar(), SizedBox(height: 24)],
+          children: [
+            const CustomHomeAppBar(),
+            SizedBox(height: 24),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'دور علي ',
+                    style: TextStyles.bold32.copyWith(
+                      color: AppColors.primaryColor,
+                      height: 1.25,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'مستقبلك',
+                    style: TextStyles.bold32.copyWith(
+                      color: AppColors.lightPrimaryColor,
+                      height: 1.25,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'هتلاقي كل الجامعات المصريه بسهوله',
+              style: TextStyles.regular14.copyWith(
+                color: Color(0xFF99A1AE),
+                height: 1.43,
+              ),
+            ),
+            SizedBox(height: 16),
+            SearchTextField(),
+            SizedBox(height: 16),
+          ],
         ),
       ),
     );
