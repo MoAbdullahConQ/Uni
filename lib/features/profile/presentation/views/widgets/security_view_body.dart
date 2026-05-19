addf56f26c680c724dc31e40ef04e06afa952660
Mu Abdullah <mohamed553231@gmail.com>
2026-05-19 03:49:53 +0300

    feat(profile): enhance SecurityViewBody layout and styling

--- lib/features/profile/presentation/views/widgets/security_view_body.dart
+++ lib/features/profile/presentation/views/widgets/security_view_body.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:uni/constants.dart';
+import 'package:uni/core/utils/app_colors.dart';
+import 'package:uni/core/utils/app_text_style.dart';
+import 'package:uni/features/profile/presentation/views/widgets/profile_header.dart';
 
 class SecurityViewBody extends StatelessWidget {
   const SecurityViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
       child: Column(
         children: [
-          SizedBox(height: kTopPadding),
-        
+          const SizedBox(height: kTopPadding),
+          ProfileHeader(
+            textHeader: 'الأمان وكلمة المرور',
+            textStyle: TextStyles.regular20.copyWith(
+              color: AppColors.primaryColor,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          const SizedBox(height: 10),
+          const Divider(height: 1, color: AppColors.borderColor),
+          const Expanded(
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(),
+            ),
+          ),
         ],
       ),
     );
