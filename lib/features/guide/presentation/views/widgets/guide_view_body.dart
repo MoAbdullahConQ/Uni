b4999544f776560b769e4de30d6eb186ef1a51ee
Mu Abdullah <mohamed553231@gmail.com>
2026-05-20 14:25:37 +0300

    feat(guide): implement GuideSearchBar

--- lib/features/guide/presentation/views/widgets/guide_view_body.dart
+++ lib/features/guide/presentation/views/widgets/guide_view_body.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:uni/constants.dart';
+import 'package:uni/core/utils/app_colors.dart';
+import 'package:uni/core/utils/app_text_style.dart';
+import 'package:uni/features/guide/presentation/views/widgets/guide_search_bar.dart';
 
 class GuideViewBody extends StatelessWidget {
   const GuideViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: kHorizontalPadding,
-          vertical: kTopPadding,
-        ),
-        child: Column(
-          
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: kTopPadding),
+
+          // Title
+          Text(
+            'دليلك الجامعي 📚',
+            style: TextStyles.bold24.copyWith(color: AppColors.primaryColor),
+          ),
+          const SizedBox(height: 16),
+
+          // Search bar
+          const GuideSearchBar(),
+          const SizedBox(height: 28),
+        ],
       ),
     );
   }
