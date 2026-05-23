646bf3b1d9ca5446d884e8f92904e2782c4e54bd
Mu Abdullah <mohamed553231@gmail.com>
2026-05-23 18:20:51 +0300

    feat(search): convert SearchViewBody to StatefulWidget and add search field with filter button

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:uni/constants.dart';
 
-class SearchViewBody extends StatelessWidget {
+import 'package:uni/core/widgets/filter_button_badge.dart';
+import 'package:uni/core/widgets/search_bar_field.dart';
+
+class SearchViewBody extends StatefulWidget {
   const SearchViewBody({super.key});
 
   @override
+  State<SearchViewBody> createState() => _SearchViewBodyState();
+}
+
+class _SearchViewBodyState extends State<SearchViewBody> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(children: [
-       ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: kHorizontalPadding,
+        vertical: kTopPadding,
+      ),
+      child: Column(
+        children: [
+          // App bar
+          SearchBarField(
+            controller: controller,
+            hintText: 'ابحث عن جامعة، كلية، أو تخصص',
+            showBackButton: true,
+            trailing: Padding(
+              padding: const EdgeInsets.only(right: 12),
+              child: FilterButtonBadge(
+                activeFiltersCount: 3,
+                onFilterTap: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
