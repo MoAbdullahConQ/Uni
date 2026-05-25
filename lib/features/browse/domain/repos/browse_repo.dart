76a842597828f83e8bddefe706abd90558e4f208
Mu Abdullah <mohamed553231@gmail.com>
2026-05-25 15:41:14 +0300

    feat(browse): create BrowseRepo interface for university data retrieval

---
+++ lib/features/browse/domain/repos/browse_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:uni/core/entities/uni_entity.dart';
+import 'package:uni/core/errors/failures.dart';
+
+abstract class BrowseRepo {
+  Future<Either<Failure, List<UniEntity>>> getUniversities({int pageNumber = 1});
+}
