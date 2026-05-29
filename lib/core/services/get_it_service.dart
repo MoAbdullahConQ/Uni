edf105f60525f8b48b3f5ba050fa4a6b13bdc742
Mu Abdullah <mohamed553231@gmail.com>
2026-05-29 03:08:38 +0300

    feat(core)(dependencies): change GetUnisUseCase registration from factory to singleton in GetIt service

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -29,7 +29,5 @@ void setupGetIt() {
   );
 
   // GetUnisUseCase
-  getIt.registerFactory<GetUnisUseCase>(
-    () => GetUnisUseCase(getIt<BrowseRepo>()),
-  );
+  getIt.registerSingleton<GetUnisUseCase>(GetUnisUseCase(getIt<BrowseRepo>()));
 }
