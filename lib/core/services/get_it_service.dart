8871b9aeb35ab13f6a3fde76c446df48f847efeb
Mu Abdullah <mohamed553231@gmail.com>
2026-05-29 03:05:22 +0300

    feat(core)(dependencies): setup GetIt for dependency injection of Dio, ApiService, BrowseRemoteDataSource, BrowseRepo, and GetUnisUseCase

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,4 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:uni/core/utils/api_service.dart';
+import 'package:uni/features/browse/data/data_sources/browse_remote_data_source.dart';
+import 'package:uni/features/browse/data/repos/browse_repo_impl.dart';
+import 'package:uni/features/browse/domain/repos/browse_repo.dart';
+import 'package:uni/features/browse/domain/use_cases/get_unis_use_case.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -7,11 +13,23 @@ void setupGetIt() {
   // Example:
   // getIt.registerLazySingleton<YourService>(() => YourServiceImplementation());
 
-  // getIt.registerSingleton<DatabaseService>(FirestoreService());
-  // getIt.registerSingleton<AuthRepo>(
-  //   AuthRepoImpl(
-  //     firebaseAuthService: getIt<FirebaseAuthService>(),
-  //     databaseService: getIt<DatabaseService>(),
-  //   ),
-  // );
+  // Dio
+  getIt.registerSingleton<Dio>(Dio());
+  // ApiService
+  getIt.registerSingleton<ApiService>(ApiService(getIt<Dio>()));
+
+  // BrowseRemoteDataSource
+  getIt.registerSingleton<BrowseRemoteDataSource>(
+    BrowseRemoteDataSourceImpl(getIt<ApiService>()),
+  );
+
+  // BrowseRepo
+  getIt.registerSingleton<BrowseRepo>(
+    BrowseRepoImpl(getIt<BrowseRemoteDataSource>()),
+  );
+
+  // GetUnisUseCase
+  getIt.registerFactory<GetUnisUseCase>(
+    () => GetUnisUseCase(getIt<BrowseRepo>()),
+  );
 }
