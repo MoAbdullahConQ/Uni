a4b2635824c21a92db73bd536d58e34864fb43ac
Mu Abdullah <mohamed553231@gmail.com>
2026-05-29 03:23:42 +0300

    feat(browse)(presentation): integrate BrowseCubit into BrowseView for improved state management and university data fetching

--- lib/features/browse/presentation/views/browse_view.dart
+++ lib/features/browse/presentation/views/browse_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uni/core/services/get_it_service.dart';
 import 'package:uni/core/widgets/ask_faheem_button.dart';
+import 'package:uni/features/browse/domain/use_cases/get_unis_use_case.dart';
+import 'package:uni/features/browse/presentation/manager/browse_cubit/browse_cubit.dart';
 import 'package:uni/features/browse/presentation/views/widgets/browse_view_body.dart';
 
 class BrowseView extends StatelessWidget {
@@ -9,10 +13,13 @@ class BrowseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      floatingActionButton: AskFaheemButton(),
-      backgroundColor: Colors.white,
-      body: SafeArea(child: BrowseViewBody()),
+    return BlocProvider(
+      create: (context) => BrowseCubit(getIt<GetUnisUseCase>())..getUnis(),
+      child: const Scaffold(
+        floatingActionButton: AskFaheemButton(),
+        backgroundColor: Colors.white,
+        body: SafeArea(child: BrowseViewBody()),
+      ),
     );
   }
 }
