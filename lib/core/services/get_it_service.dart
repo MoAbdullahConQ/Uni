4605e4cf3cc268105fd75af370f32579929e103e
Mu Abdullah <mohamed553231@gmail.com>
2026-05-29 06:25:30 +0300

    feat(core)(dependencies): update setupGetIt to register SharedPreferences singleton and temporarily set a token for development purposes

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:uni/core/services/shared_preferences_singleton.dart';
 import 'package:uni/core/utils/api_service.dart';
 import 'package:uni/features/browse/data/data_sources/browse_remote_data_source.dart';
 import 'package:uni/features/browse/data/repos/browse_repo_impl.dart';
@@ -8,13 +9,17 @@ import 'package:uni/features/browse/domain/use_cases/get_unis_use_case.dart';
 
 final GetIt getIt = GetIt.instance;
 
-void setupGetIt() {
+Future<void> setupGetIt() async {
   // Register your services here
   // Example:
   // getIt.registerLazySingleton<YourService>(() => YourServiceImplementation());
 
   // Dio
   getIt.registerSingleton<Dio>(Dio());
+
+  // TODO: remove before production
+  await Prefs.setString('token', '277|91C9QiwRjkiM2YG3huAPAsA1NBI7mKqb8dQVZvre2857f569');
+
   // ApiService
   getIt.registerSingleton<ApiService>(ApiService(getIt<Dio>()));
 
