a45073d9b4da9caa76aa7d3e8502fa8f94ed95bb
Mu Abdullah <mohamed553231@gmail.com>
2026-05-30 09:34:07 +0300

    feat(search)(domain): add SearchRepo interface for search functionality with query and filter parameters

---
+++ lib/features/search/domain/repos/search_repo.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:uni/core/errors/failures.dart';
+import 'package:uni/core/entities/unis_response.dart';
+import 'package:uni/features/search/domain/entities/search_filter_entity.dart';
+
+abstract class SearchRepo {
+  Future<Either<Failure, UnisResponse>> search({
+    required String query,
+    required SearchFilterEntity filter,
+    String? cursor,
+  });
+}
