7ae11bb92d80221ebfaf47ca43d0f885309ec21e
Mu Abdullah <mohamed553231@gmail.com>
2026-05-30 04:20:32 +0300

    feat(home)(presentation): integrate FavCubit with BlocProvider in MainView for favorite management

--- lib/features/home/presentation/views/main_view.dart
+++ lib/features/home/presentation/views/main_view.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uni/core/services/get_it_service.dart';
 import 'package:uni/core/widgets/ask_faheem_button.dart';
+import 'package:uni/features/fav/domain/use_cases/add_to_fav_use_case.dart';
+import 'package:uni/features/fav/domain/use_cases/get_favs_use_case.dart';
+import 'package:uni/features/fav/domain/use_cases/remove_from_fav_use_case.dart';
+import 'package:uni/features/fav/presentation/manager/fav_cubit/fav_cubit.dart';
 import 'package:uni/features/fav/presentation/views/widgets/fav_view_body.dart';
 import 'package:uni/features/guide/presentation/views/widgets/guide_view_body.dart';
 import 'package:uni/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart';
@@ -21,16 +27,21 @@ class _MainViewState extends State<MainView> {
   List<Widget> get views => [
     const HomeViewBody(),
     const GuideViewBody(),
-    const FavViewBody(),
+    BlocProvider(
+      create: (context) => FavCubit(
+        getFavsUseCase: getIt<GetFavsUseCase>(),
+        addToFavUseCase: getIt<AddToFavUseCase>(),
+        removeFromFavUseCase: getIt<RemoveFromFavUseCase>(),
+      )..getFavs(),
+      child: const FavViewBody(),
+    ),
     const ProfileViewBody(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: currentIndex != 3
-          ? const AskFaheemButton()
-          : null,
+      floatingActionButton: currentIndex != 3 ? const AskFaheemButton() : null,
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: currentIndex,
         onIndexChanged: (int index) {
