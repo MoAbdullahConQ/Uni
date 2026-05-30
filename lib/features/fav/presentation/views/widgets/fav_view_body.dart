5eb68cf0ccb68e02643f95a26f6b47c540acc242
Mu Abdullah <mohamed553231@gmail.com>
2026-05-30 04:20:45 +0300

    feat(fav)(presentation): implement infinite scroll in FavViewBody with Cubit integration for loading more favorites

--- lib/features/fav/presentation/views/widgets/fav_view_body.dart
+++ lib/features/fav/presentation/views/widgets/fav_view_body.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uni/constants.dart';
-import 'package:uni/core/helper_functions/getDummyEntities.dart';
 import 'package:uni/core/utils/app_colors.dart';
 import 'package:uni/core/utils/app_text_style.dart';
+import 'package:uni/core/widgets/custom_error_widget.dart';
 import 'package:uni/core/widgets/search_bar_field.dart';
 import 'package:uni/core/entities/uni_entity.dart';
 import 'package:uni/core/widgets/uni_count_header.dart';
 import 'package:uni/core/widgets/uni_filter_tab_bar.dart';
 import 'package:uni/core/widgets/uni_list_widget.dart';
+import 'package:uni/features/fav/presentation/manager/fav_cubit/fav_cubit.dart';
 
 class FavViewBody extends StatefulWidget {
   const FavViewBody({super.key});
@@ -18,17 +20,42 @@ class FavViewBody extends StatefulWidget {
 
 class _FavViewBodyState extends State<FavViewBody> {
   String selectedFilter = 'الكل';
+  final ScrollController _scrollController = ScrollController();
 
-  // TODO: replace with real data from cubit
-  final List<UniEntity> allFavUniEntities = getDummyUniEntities();
-  List<UniEntity> get selectedFilterFavUniEntities {
-    if (selectedFilter == 'الكل') return allFavUniEntities;
-    return allFavUniEntities.where((e) => e.type == selectedFilter).toList();
+  List<UniEntity> getFilteredEntitiesList(List<UniEntity> unis) {
+    if (selectedFilter == 'الكل') return unis;
+    return unis.where((e) => e.type == selectedFilter).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (!mounted) return;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (currentScroll >= maxScroll * 0.8) {
+      final state = context.read<FavCubit>().state;
+      if (state is! FavPaginationLoading) {
+        context.read<FavCubit>().loadMore();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      controller: _scrollController,
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: kHorizontalPadding,
@@ -60,23 +87,82 @@ class _FavViewBodyState extends State<FavViewBody> {
             ),
             const SizedBox(height: 24),
 
+            // BlocBuilder
+            FavHeaderAndListBlocBuilder(
+              getFilteredEntitiesList: getFilteredEntitiesList,
+            ),
+            // const SizedBox(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FavHeaderAndListBlocBuilder extends StatelessWidget {
+  const FavHeaderAndListBlocBuilder({
+    super.key,
+    required this.getFilteredEntitiesList,
+  });
+
+  final List<UniEntity> Function(List<UniEntity>) getFilteredEntitiesList;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FavCubit, FavState>(
+      builder: (context, state) {
+        if (state is FavFailure) {
+          return CustomErrorWidget(message: state.errMessage);
+        }
+
+        List<UniEntity> currentUnis = [];
+        bool isPaginationLoading = false;
+        String? paginationError;
+
+        if (state is FavSuccess) {
+          currentUnis = state.uniEntities;
+        } else if (state is FavPaginationLoading) {
+          currentUnis = state.currentUnis;
+          isPaginationLoading = true;
+        } else if (state is FavPaginationFailure) {
+          currentUnis = state.currentUnis;
+          paginationError = state.errMessage;
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final filteredEntitiesList = getFilteredEntitiesList(currentUnis);
+
+        return Column(
+          children: [
             // Count + sort
             UniCountHeader(
-              count: selectedFilterFavUniEntities.length,
+              count: filteredEntitiesList.length,
               label: 'جامعات مفضلة',
             ),
             const SizedBox(height: 16),
 
             // List
             UniListWidget(
-              selectedFilterUniEntities: selectedFilterFavUniEntities,
-              itemCount: selectedFilterFavUniEntities.length,
-              onDelete: () {},
+              selectedFilterUniEntities: filteredEntitiesList,
+              itemCount: filteredEntitiesList.length,
+              // onDelete: (id) => context.read<FavCubit>().removeFromFav(id),
             ),
-            // const SizedBox(height: 32),
+
+            if (isPaginationLoading)
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: Center(child: CircularProgressIndicator()),
+              ),
+
+            if (paginationError != null)
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: CustomErrorWidget(message: paginationError),
+              ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
