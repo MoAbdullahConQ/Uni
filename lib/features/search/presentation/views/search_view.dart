0dbc9791a92b37184401721f44197d7fb2dab4ec
Mu Abdullah <mohamed553231@gmail.com>
2026-05-30 09:46:50 +0300

    feat(search)(presentation): wrap SearchView in BlocProvider for SearchCubit instantiation

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uni/core/services/get_it_service.dart';
 import 'package:uni/core/widgets/ask_faheem_button.dart';
+import 'package:uni/features/search/domain/use_cases/search_unis_use_case.dart';
+import 'package:uni/features/search/presentation/manager/search_cubit/search_cubit.dart';
 import 'package:uni/features/search/presentation/views/widgets/search_view_body.dart';
 
 class SearchView extends StatelessWidget {
@@ -9,13 +13,16 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFF4FAE8),
-      floatingActionButton: Padding(
-        padding: EdgeInsets.only(bottom: 40),
-        child: AskFaheemButton(),
+    return BlocProvider(
+      create: (context) => SearchCubit(getIt<SearchUnisUseCase>()),
+      child: const Scaffold(
+        backgroundColor: Color(0xFFF4FAE8),
+        floatingActionButton: Padding(
+          padding: EdgeInsets.only(bottom: 40),
+          child: AskFaheemButton(),
+        ),
+        body: SafeArea(child: SearchViewBody()),
       ),
-      body: SafeArea(child: SearchViewBody()),
     );
   }
 }
