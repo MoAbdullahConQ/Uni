77c1c08c3c0e70dcdaa36edbb86287dd4d4ff401
Mu Abdullah <mohamed553231@gmail.com>
2026-05-30 06:38:09 +0300

    feat(browse)(presentation): add FavCubit to MultiBlocProvider in BrowseView for favorite management integration

--- lib/features/browse/presentation/views/browse_view.dart
+++ lib/features/browse/presentation/views/browse_view.dart
@@ -5,6 +5,10 @@ import 'package:uni/core/widgets/ask_faheem_button.dart';
 import 'package:uni/features/browse/domain/use_cases/get_unis_use_case.dart';
 import 'package:uni/features/browse/presentation/manager/browse_cubit/browse_cubit.dart';
 import 'package:uni/features/browse/presentation/views/widgets/browse_view_body.dart';
+import 'package:uni/features/fav/domain/use_cases/add_to_fav_use_case.dart';
+import 'package:uni/features/fav/domain/use_cases/get_favs_use_case.dart';
+import 'package:uni/features/fav/domain/use_cases/remove_from_fav_use_case.dart';
+import 'package:uni/features/fav/presentation/manager/fav_cubit/fav_cubit.dart';
 
 class BrowseView extends StatelessWidget {
   const BrowseView({super.key});
@@ -13,8 +17,19 @@ class BrowseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => BrowseCubit(getIt<GetUnisUseCase>())..getUnis(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => BrowseCubit(getIt<GetUnisUseCase>())..getUnis(),
+        ),
+        BlocProvider(
+          create: (context) => FavCubit(
+            getFavsUseCase: getIt<GetFavsUseCase>(),
+            addToFavUseCase: getIt<AddToFavUseCase>(),
+            removeFromFavUseCase: getIt<RemoveFromFavUseCase>(),
+          )..getFavs(),
+        ),
+      ],
       child: const Scaffold(
         floatingActionButton: AskFaheemButton(),
         backgroundColor: Colors.white,
