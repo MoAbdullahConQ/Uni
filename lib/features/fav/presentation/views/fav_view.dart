4fc4d787473f5c4e4510b066bdedb9eecd54b586
Mu Abdullah <mohamed553231@gmail.com>
2026-05-30 03:53:02 +0300

    feat(fav)(presentation): integrate FavCubit with BlocProvider in FavView for state management

--- lib/features/fav/presentation/views/fav_view.dart
+++ lib/features/fav/presentation/views/fav_view.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uni/core/services/get_it_service.dart';
+import 'package:uni/features/fav/domain/use_cases/add_to_fav_use_case.dart';
+import 'package:uni/features/fav/domain/use_cases/get_favs_use_case.dart';
+import 'package:uni/features/fav/domain/use_cases/remove_from_fav_use_case.dart';
+import 'package:uni/features/fav/presentation/manager/fav_cubit/fav_cubit.dart';
 import 'package:uni/features/fav/presentation/views/widgets/fav_view_body.dart';
 
 class FavView extends StatelessWidget {
@@ -8,6 +14,13 @@ class FavView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: FavViewBody()));
+    return BlocProvider(
+      create: (context) => FavCubit(
+        getFavsUseCase: getIt<GetFavsUseCase>(),
+        addToFavUseCase: getIt<AddToFavUseCase>(),
+        removeFromFavUseCase: getIt<RemoveFromFavUseCase>(),
+      )..getFavs(),
+      child: const Scaffold(body: SafeArea(child: FavViewBody())),
+    );
   }
 }
