375625c7a110d646d045021a78493bae318c17d2
Mu Abdullah <mohamed553231@gmail.com>
2026-06-06 17:49:14 +0300

    feat(trending)(entity): add TrendingUniEntity for encapsulating trending university data

---
+++ lib/core/entities/trending_uni_entity.dart
@@ -0,0 +1,11 @@
+class TrendingUniEntity {
+  final String name;
+  final String worldRanking;
+  final String? logoPath;
+
+  TrendingUniEntity({
+    required this.name,
+    required this.worldRanking,
+    this.logoPath,
+  });
+}
