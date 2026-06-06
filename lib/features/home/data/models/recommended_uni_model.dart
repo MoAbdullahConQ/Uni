26a878abd7267a19c351f934a0fc108c3a55eabf
Mu Abdullah <mohamed553231@gmail.com>
2026-06-07 01:11:49 +0300

    feat(home)(models): add RecommendedUniModel for mapping recommended university data from JSON

---
+++ lib/features/home/data/models/recommended_uni_model.dart
@@ -0,0 +1,25 @@
+import 'package:uni/features/home/domain/entities/recommended_uni_entity.dart';
+
+class RecommendedUniModel extends RecommendedUniEntity {
+  RecommendedUniModel({
+    required super.name,
+    required super.location,
+    required super.imagePath,
+    super.logoPath,
+    required super.type,
+    required super.rate,
+    required super.studentsCount,
+  });
+
+  factory RecommendedUniModel.fromJson(Map<String, dynamic> json) {
+    return RecommendedUniModel(
+      name: json['name'] ?? '',
+      location: json['location'] ?? '',
+      imagePath: json['background_image_url'] ?? '',
+      logoPath: json['avatar_url'] as String?,
+      type: json['type'] ?? '',
+      rate: json['rate'] ?? '0.00',
+      studentsCount: json['students_count'] ?? 0,
+    );
+  }
+}
