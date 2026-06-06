d47f9701a22e8e0fc8f6fd28f1348649d89e80c9
Mu Abdullah <mohamed553231@gmail.com>
2026-06-07 01:14:16 +0300

    feat(home)(presentation): update UniImageSection to use network images and add error handling for image loading

--- lib/features/home/presentation/views/widgets/uni_image_section.dart
+++ lib/features/home/presentation/views/widgets/uni_image_section.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:uni/core/utils/app_colors.dart';
-import 'package:uni/core/utils/app_images.dart';
 import 'package:uni/core/widgets/type_badge_widget.dart';
 import 'package:uni/features/home/domain/entities/recommended_uni_entity.dart';
 
@@ -14,14 +13,19 @@ class UniImageSection extends StatelessWidget {
     return Stack(
       clipBehavior: Clip.none,
       children: [
-        // University Image
+        // University Background Image
         ClipRRect(
           borderRadius: const BorderRadius.vertical(top: Radius.circular(23)),
-          child: Image.asset(
+          child: Image.network(
             recommendedUniEntity.imagePath,
             height: 130,
             width: double.infinity,
             fit: BoxFit.cover,
+            errorBuilder: (_, __, ___) => Container(
+              height: 130,
+              color: AppColors.primaryColor.withOpacity(0.1),
+              child: const Icon(Icons.school, color: AppColors.primaryColor),
+            ),
           ),
         ),
 
@@ -35,14 +39,11 @@ class UniImageSection extends StatelessWidget {
               color: Colors.white.withOpacity(0.9),
               borderRadius: BorderRadius.circular(8),
             ),
-            child: const Icon(
-              Icons.bookmark_border,
-              // size: 22,
-              color: Colors.grey,
-            ),
+            child: const Icon(Icons.bookmark_border, color: Colors.grey),
           ),
         ),
 
+        // Type Badge
         Positioned(
           bottom: -10,
           left: 10,
@@ -68,10 +69,14 @@ class UniImageSection extends StatelessWidget {
                 ),
               ],
             ),
-            child: Image.asset(
-              recommendedUniEntity.logoPath ?? Assets.imagesCairoUni,
-              fit: BoxFit.contain,
-            ),
+            child: recommendedUniEntity.logoPath != null
+                ? Image.network(
+                    recommendedUniEntity.logoPath!,
+                    fit: BoxFit.contain,
+                    errorBuilder: (_, __, ___) =>
+                        const Icon(Icons.school, color: AppColors.primaryColor),
+                  )
+                : const Icon(Icons.school, color: AppColors.primaryColor),
           ),
         ),
       ],
