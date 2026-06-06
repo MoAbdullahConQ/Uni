da86761bab2b65551d8e3599ce4121a0ab476492
Mu Abdullah <mohamed553231@gmail.com>
2026-06-06 18:03:37 +0300

    feat(search)(presentation): integrate TrendingCubit in SearchView and update SearchContentBlocBuilder for fetching trending searches

--- lib/features/search/presentation/views/search_view.dart
+++ lib/features/search/presentation/views/search_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uni/core/cubits/trending_cubit/trending_cubit.dart';
 import 'package:uni/core/services/get_it_service.dart';
 import 'package:uni/core/widgets/ask_faheem_button.dart';
 import 'package:uni/features/search/domain/use_cases/search_unis_use_case.dart';
@@ -13,8 +14,13 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SearchCubit(getIt<SearchUnisUseCase>()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => SearchCubit(getIt<SearchUnisUseCase>()),
+        ),
+        BlocProvider.value(value: getIt<TrendingCubit>()),
+      ],
       child: const Scaffold(
         backgroundColor: Color(0xFFF4FAE8),
         floatingActionButton: Padding(
--- lib/features/search/presentation/views/widgets/search_content_bloc_builder.dart
+++ lib/features/search/presentation/views/widgets/search_content_bloc_builder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uni/core/cubits/trending_cubit/trending_cubit.dart';
 import 'package:uni/core/helper_functions/getDummyEntities.dart';
 import 'package:uni/features/search/presentation/manager/search_cubit/search_cubit.dart';
 import 'package:uni/features/search/presentation/views/widgets/search_empty_widget.dart';
@@ -21,17 +22,26 @@ class SearchContentBlocBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SearchCubit, SearchCubitState>(
-      builder: (context, state) {
-        if (state is SearchInitial) {
-          return SearchHomeWidget(
-            recentSearches: getDummyRecentSearches(),
-            trendingSearches: getDummyTrendingSearches(),
-            onClearAll: () {},
-            onSearchTap: onSearchTap,
+      builder: (context, searchState) {
+        if (searchState is SearchInitial) {
+          return BlocBuilder<TrendingCubit, TrendingState>(
+            builder: (context, trendingState) {
+              final List<String> trendingSearches =
+                  trendingState is TrendingSuccess
+                  ? trendingState.trendingSearches
+                  : getDummyTrendingSearches();
+
+              return SearchHomeWidget(
+                recentSearches: getDummyRecentSearches(),
+                trendingSearches: trendingSearches,
+                onClearAll: () {},
+                onSearchTap: onSearchTap,
+              );
+            },
           );
         }
 
-        if (state is SearchEmpty) {
+        if (searchState is SearchEmpty) {
           return SearchEmptyWidget(onClearFilters: onClearFilters);
         }
 
