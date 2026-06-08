2ea51805c220cea8776a72a88841fd30afd6ed4b
Mu Abdullah <mohamed553231@gmail.com>
2026-06-08 04:24:15 +0300

    feat(notifications)(presentation): refresh data change NotificationsView from StatelessWidget to StatefulWidget to fetch notifications in initState

--- lib/features/notifications/presentation/views/notifications_view.dart
+++ lib/features/notifications/presentation/views/notifications_view.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:uni/core/services/get_it_service.dart';
+import 'package:uni/features/notifications/presentation/manager/notifications_cubit/notifications_cubit.dart';
 import 'package:uni/features/notifications/presentation/views/widgets/notifications_view_body.dart';
 
-class NotificationsView extends StatelessWidget {
+class NotificationsView extends StatefulWidget {
   const NotificationsView({super.key});
 
   static const routeName = 'notifications';
+
+  @override
+  State<NotificationsView> createState() => _NotificationsViewState();
+}
+
+class _NotificationsViewState extends State<NotificationsView> {
+  @override
+  void initState() {
+    super.initState();
+    getIt<NotificationsCubit>().getNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
