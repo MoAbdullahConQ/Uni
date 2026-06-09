bef767992cf3ce39d3b6b419e9cec36551793e56
Mu Abdullah <mohamed553231@gmail.com>
2026-06-09 16:14:33 +0300

    feat(guide)(presentation): create GuideArticleDetailView for article detail display

---
+++ lib/features/guide/presentation/views/guide_article_detail_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:uni/features/guide/domain/entities/guide_article_entity.dart';
+// import 'package:uni/features/guide/presentation/views/widgets/guide_article_detail_view_body.dart';
+
+class GuideArticleDetailView extends StatelessWidget {
+  const GuideArticleDetailView({super.key, required this.article});
+
+  static const String routeName = 'guide_article_detail_view';
+
+  final GuideArticleEntity article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        // child: GuideArticleDetailViewBody(article: article),
+      ),
+    );
+  }
+}
