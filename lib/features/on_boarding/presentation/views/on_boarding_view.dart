03665ed459664b46947cfb30e5a511330bb7bf2f
Mu Abdullah <mohamed553231@gmail.com>
2026-06-17 14:21:46 +0300

    fix(onboarding)(navigation): update navigation to LoginView after onboarding completion

--- lib/features/on_boarding/presentation/views/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uni/constants.dart';
 import 'package:uni/core/services/shared_preferences_singleton.dart';
-import 'package:uni/features/home/presentation/views/main_view.dart';
+import 'package:uni/features/auth/presentation/views/login_view.dart';
 import 'package:uni/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart';
 
 class OnBoardingView extends StatelessWidget {
@@ -18,7 +18,7 @@ class OnBoardingView extends StatelessWidget {
           child: OnBoardingViewBody(
             onDone: () {
               Prefs.setBool(kIsOnBoardingViewSeenKey, true);
-              Navigator.pushReplacementNamed(context, MainView.routeName);
+              Navigator.pushReplacementNamed(context, LoginView.routeName);
             },
           ),
         ),
