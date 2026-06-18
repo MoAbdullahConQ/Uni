7cfc8f98bbea2352d59fb736bbdaf4dfe79501e0
Mu Abdullah <mohamed553231@gmail.com>
2026-06-19 00:38:57 +0300

    feat(profile)(presentation): integrate BlocBuilder for ProfileCubit in ProfileViewBody to manage state

--- lib/features/profile/presentation/views/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/profile_view_body.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uni/constants.dart';
+import 'package:uni/core/services/get_it_service.dart';
 import 'package:uni/core/utils/app_colors.dart';
 import 'package:uni/core/utils/app_text_style.dart';
+import 'package:uni/features/profile/presentation/manager/profile_cubit/profile_cubit.dart';
 import 'package:uni/features/profile/presentation/views/widgets/profile_avatar_section.dart';
 import 'package:uni/features/profile/presentation/views/widgets/profile_header.dart';
 import 'package:uni/features/profile/presentation/views/widgets/profile_logout_button.dart';
@@ -13,44 +16,49 @@ class ProfileViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-      child: Column(
-        children: [
-          const SizedBox(height: kTopPadding),
-          ProfileHeader(
-            textHeader: 'الملف الشخصي',
-            textStyle: TextStyles.bold20.copyWith(
-              color: AppColors.primaryColor,
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Divider(height: 1, color: AppColors.borderColor),
-          const Expanded(
-            child: SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 24),
-                  ProfileAvatarSection(
-                    name: 'مجدي عبد الغني',
-                    email: 'ahmed.m@example.com',
-                    role: 'طالب - علمي رياضة',
-                  ),
-                  SizedBox(height: 24),
-                  ProfileMenuSection(),
-                  SizedBox(height: 24),
-                  ProfileLogoutButton(),
-                  SizedBox(height: 24),
-                  VersionInfo(),
-                  SizedBox(height: 32),
-                ],
+    return BlocBuilder<ProfileCubit, ProfileState>(
+      bloc: getIt<ProfileCubit>(),
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+          child: Column(
+            children: [
+              const SizedBox(height: kTopPadding),
+              ProfileHeader(
+                textHeader: 'الملف الشخصي',
+                textStyle: TextStyles.bold20.copyWith(
+                  color: AppColors.primaryColor,
+                ),
               ),
-            ),
+              const SizedBox(height: 10),
+              const Divider(height: 1, color: AppColors.borderColor),
+              const Expanded(
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 24),
+                      ProfileAvatarSection(
+                        name: 'مجدي عبد الغني',
+                        email: 'ahmed.m@example.com',
+                        role: 'طالب - علمي رياضة',
+                      ),
+                      SizedBox(height: 24),
+                      ProfileMenuSection(),
+                      SizedBox(height: 24),
+                      ProfileLogoutButton(),
+                      SizedBox(height: 24),
+                      VersionInfo(),
+                      SizedBox(height: 32),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
