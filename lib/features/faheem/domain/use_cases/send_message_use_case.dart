46987c3e04d692854b11dfb727345c28b39acebb
Mu Abdullah <mohamed553231@gmail.com>
2026-06-19 17:29:15 +0300

    feat(faheem)(use_case): implement SendMessageUseCase for sending chat messages

---
+++ lib/features/faheem/domain/use_cases/send_message_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:uni/core/errors/failures.dart';
+import 'package:uni/features/faheem/domain/entities/chat_message_entity.dart';
+import 'package:uni/features/faheem/domain/repos/faheem_repo.dart';
+
+class SendMessageUseCase {
+  final FaheemRepo faheemRepo;
+
+  SendMessageUseCase(this.faheemRepo);
+
+  Future<Either<Failure, ChatMessageEntity>> call({
+    required String message,
+  }) {
+    return faheemRepo.sendMessage(message: message);
+  }
+}
