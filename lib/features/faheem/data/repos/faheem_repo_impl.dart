f8d147574a1a3d808984c205e92ff0410e5e45d5
Mu Abdullah <mohamed553231@gmail.com>
2026-06-23 01:51:53 +0300

    feat(faheem)(domain): extend FaheemRepo with conversation retrieval methods

--- lib/features/faheem/data/repos/faheem_repo_impl.dart
+++ lib/features/faheem/data/repos/faheem_repo_impl.dart
@@ -3,6 +3,8 @@ import 'package:dio/dio.dart';
 import 'package:uni/core/errors/failures.dart';
 import 'package:uni/features/faheem/data/data_sources/faheem_remote_data_source.dart';
 import 'package:uni/features/faheem/domain/entities/chat_message_entity.dart';
+import 'package:uni/features/faheem/domain/entities/conversation_details_entity.dart';
+import 'package:uni/features/faheem/domain/entities/conversation_entity.dart';
 import 'package:uni/features/faheem/domain/repos/faheem_repo.dart';
 
 class FaheemRepoImpl implements FaheemRepo {
@@ -13,6 +15,7 @@ class FaheemRepoImpl implements FaheemRepo {
   @override
   Future<Either<Failure, ChatMessageEntity>> sendMessage({
     required String message,
+    int? conversationId
   }) async {
     try {
       final result =
@@ -21,5 +24,17 @@ class FaheemRepoImpl implements FaheemRepo {
     } on DioException catch (e) {
       return left(ServerFailure.fromDioError(e));
     }
+  }
+
+  @override
+  Future<Either<Failure, ConversationDetailsEntity>> getConversationMessages(int id) {
+    // TODO: implement getConversationMessages
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<ConversationEntity>>> getConversations() {
+    // TODO: implement getConversations
+    throw UnimplementedError();
   }
 }
--- lib/features/faheem/domain/repos/faheem_repo.dart
+++ lib/features/faheem/domain/repos/faheem_repo.dart
@@ -1,9 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:uni/core/errors/failures.dart';
 import 'package:uni/features/faheem/domain/entities/chat_message_entity.dart';
+import 'package:uni/features/faheem/domain/entities/conversation_details_entity.dart';
+import 'package:uni/features/faheem/domain/entities/conversation_entity.dart';
 
 abstract class FaheemRepo {
   Future<Either<Failure, ChatMessageEntity>> sendMessage({
     required String message,
+    int? conversationId,
   });
+  Future<Either<Failure, List<ConversationEntity>>> getConversations();
+  Future<Either<Failure, ConversationDetailsEntity>> getConversationMessages(
+    int id,
+  );
 }
